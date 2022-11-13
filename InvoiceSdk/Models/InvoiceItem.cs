06cdc29890b08d01294ddca43d692a8c4c87dd28
Nikolas Andreou <nickandreou94@gmail.com>
2022-11-14 01:53:06 +0200

    Create relation between InvoiceItem and Invoice model

--- InvoiceSdk/Models/InvoiceItem.cs
+++ InvoiceSdk/Models/InvoiceItem.cs
@@ -3,6 +3,7 @@
     public class InvoiceItem
     {
         public int Id { get; set; }
+        public Guid InvoiceId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int Quantity { get; set; }
