84fa0c13beeda66b5fba65bb07d255e9e6e7dae7
Nikolas Andreou <nickandreou94@gmail.com>
2023-04-18 00:23:43 +0300

    Add InvoiceRenderer documentation and wrapper Task method

--- InvoiceSdk/Renderer/InvoiceRenderer.cs
+++ InvoiceSdk/Renderer/InvoiceRenderer.cs
@@ -3,13 +3,26 @@ using InvoiceSdk.Renderer.Components;
 using InvoiceSdk.Renderer.Configuration;
 using QuestPDF.Infrastructure;
 
-namespace InvoiceSdk.Renderer
+namespace InvoiceSdk.Renderer;
+
+public class InvoiceRenderer : IInvoiceRenderer
 {
-    public class InvoiceRenderer : IInvoiceRenderer
-    {
-        public IDocument RenderInvoice(Invoice invoice, InvoiceConfiguration configuration)
-        {
-            return new InvoiceDocument(invoice, configuration);
-        }
-    }
+    /// <summary>
+    /// Renders an invoice document based on the provided Invoice model and configuration.
+    /// </summary>
+    /// <param name="invoice">The Invoice model to render the document from.</param>
+    /// <param name="configuration">The configuration to be passed to the internal invoice renderer.</param>
+    /// <returns></returns>
+    public IDocument RenderInvoice(Invoice invoice, InvoiceConfiguration configuration) => new InvoiceDocument(invoice, configuration);
+    
+    /// <summary>
+    /// Renders an invoice document based on the provided Invoice model and configuration.
+    /// </summary>
+    /// <remarks>
+    /// This method only wraps the <see cref="RenderInvoice">RenderInvoice()</see> method inside a Task.Run() statement. It is therefore, not truly asynchronous.
+    /// </remarks>
+    /// <param name="invoice">The Invoice model to render the document from.</param>
+    /// <param name="configuration">The configuration to be passed to the internal invoice renderer.</param>
+    /// <returns></returns>
+    public Task<IDocument> RenderInvoiceAsync(Invoice invoice, InvoiceConfiguration configuration) => Task.Run(() => RenderInvoice(invoice, configuration));
 }
